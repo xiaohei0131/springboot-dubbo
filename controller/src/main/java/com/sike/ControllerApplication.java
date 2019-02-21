67ed7aa1da94a29d3c24c9a51eefbfc347e43077
zhouqiang <huangxin@nj.fiberhome.com.cn>
2019-02-21 10:05:43 +0800

    增加druid连接池以依赖及配置

--- controller/src/main/java/com/sike/ControllerApplication.java
+++ controller/src/main/java/com/sike/ControllerApplication.java
@@ -1,12 +1,13 @@
 package com.sike;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import com.sike.permission.annotation.EnablePermissionConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 @EnableDubboConfiguration
 @EnablePermissionConfiguration
 public class ControllerApplication {
